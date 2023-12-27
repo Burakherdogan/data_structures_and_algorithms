d8a6991b2f71c8cadacd39be8e9ddab758e46047
Burakherdogan <erdoganburakhasan@gmail.com>
2023-12-27 22:19:48 +0300

    created stack_via_ll

---
+++ structures/stack.py
@@ -0,0 +1,37 @@
+class Node:
+    #constructor
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Stack: #has LIFO
+    #constructor
+    def __init__(self,value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+        
+    ##########################################    
+    def print_stack(self): #O(n)
+        temp = self.top
+        while temp is not None: 
+            print(temp.value)
+            temp = temp.next
+    ##########################################
+    def push(self, value): #O(1)
+        new_node = Node(value)
+        if self.top is None:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+    ##########################################
+    def pop(self):
+        if self.top is None:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
